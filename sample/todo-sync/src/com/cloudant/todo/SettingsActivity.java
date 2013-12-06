02d360854e9b59d3c24a7f1c93d8aa687a3b459d
Dongsheng Wang <dongsheng@gmail.com>
2013-12-06 14:49:31 -0500

    Show current setting value

--- sample/todo-sync/src/com/cloudant/todo/SettingsActivity.java
+++ sample/todo-sync/src/com/cloudant/todo/SettingsActivity.java
@@ -1,13 +1,38 @@
 package com.cloudant.todo;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        SharedPreferences prefs = this.getPreferenceScreen().getSharedPreferences();
+        for(String key : prefs.getAll().keySet()) {
+            EditTextPreference prefText = (EditTextPreference) findPreference(key);
+            prefText.setSummary(prefs.getString(key, ""));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.getPreferenceManager().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+        Preference pref = findPreference(key);
+        if (pref instanceof EditTextPreference) {
+            EditTextPreference etp = (EditTextPreference) pref;
+            pref.setSummary(etp.getText());
+        }
     }
 }
